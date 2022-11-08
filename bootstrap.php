27affb2f5027957b39a8fea6256b48ef55a13a34
Thaddeus Bibbs <tiger2380@gmail.com>
2022-11-08 17:06:24 -0600

    removed composer

---
+++ bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+define('APP_PATH', __DIR__.'/App/');
+spl_autoload_register('autoLoader');
+
+function autoLoader($className) {
+    $paths = [
+        APP_PATH.'Controllers/',
+        APP_PATH.'Core/',
+        APP_PATH.'Middleware/',
+        APP_PATH.'Models/',
+    ];
+
+    $filepath = __DIR__.'/'.str_replace('\\', '/', $className).'.php';
+    if(file_exists($filepath)) {
+        require_once $filepath;
+    } else {
+        foreach ($paths as $path) {
+            $filepath = $path.str_replace('\\', '/', $className).'.php';
+            if (file_exists($filepath)) {
+                require_once $filepath;
+                break;
+            }
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
-    require __DIR__ . '/../vendor/autoload.php';
+    require __DIR__ . '/../bootstrap.php';
+    //require __DIR__ . '/../vendor/autoload.php';
 
     (new App\Core\App())->run();
