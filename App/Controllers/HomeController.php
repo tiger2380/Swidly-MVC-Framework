574bac394185c22f7024c0cffc26aa0719921d2a
Thaddeus Bibbs <tiger2380@gmail.com>
2022-11-20 12:07:47 -0600

    updates

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use App\Core\Attributes\Middleware;
 use App\Core\Controller;
 use App\Core\Attributes\Route;
-use App\Middleware\AuthMiddleware;
 
 class HomeController extends Controller {
 
