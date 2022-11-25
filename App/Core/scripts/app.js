3839c4c36a5e118d91450078bbbd3486362c8eb4
Thaddeus Bibbs <tiger2380@gmail.com>
2022-11-25 16:17:32 -0600

    updates

--- App/Core/scripts/app.js
+++ App/Core/scripts/app.js
@@ -4,9 +4,18 @@ const options = {
     'onBeforeFetch': () => {
         const div = document.createElement('div');
         div.classList.add('loader');
-        Object.assign(div.style, { width: '100%', height: '5px', backgroundColor: 'red', top: '0', left: '0', position: 'fixed', zIndex: 10 });
-
+        Object.assign(div.style, { width: '0%', height: '5px', backgroundColor: 'rgba(255, 0, 0, 0.8', top: '0', left: '0', position: 'fixed', zIndex: 10 });
         document.body.appendChild(div);
+
+        const interval = setInterval(() => {
+            let width = parseInt(div.style.width);
+            if(width <= 80) {
+                width += (Math.random() * 20);
+                div.style.width = width +'%';
+            } else {
+                clearInterval(interval);
+            }
+        }, 1000);
     },
     'onAfterFetch': () => {
         if(document.querySelector('.loader')) {
