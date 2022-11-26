7ff86521a2472929cdc258c62eb148d26a1c2bf9
Thaddeus Bibbs <tiger2380@gmail.com>
2022-11-25 21:21:21 -0600

    updates

--- App/themes/single_page/index.php
+++ App/themes/single_page/index.php
@@ -9,9 +9,8 @@
 <body>
     <a href="/" data-sp-link>Home</a> | <a href="/posts" data-sp-link>Posts</a> | <a href="/about" data-sp-link>About</a> | <a href="/contact" data-sp-link>Contact</a> | 
 
-    <?php if(App\Core\App::getConfig('app::single_page', false)): ?>
-        <div id="app"></div>
-        <?= \App\Core\App::load_single_page(); ?>
-    <?php endif; ?>
+    <div id="app"></div>
+     
+    <?= \App\Core\App::load_single_page(); ?>
 </body>
 </html>
