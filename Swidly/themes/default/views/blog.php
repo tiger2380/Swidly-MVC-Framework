056d412df82b008e6251624ef6718589c0d0fd6f
Thaddeus Bibbs <tiger2380@gmail.com>
2025-05-23 03:56:51 +0000

    updates

--- Swidly/themes/default/views/blog.php
+++ Swidly/themes/default/views/blog.php
@@ -1,4 +1,4 @@
 <?php use Swidly\Core\Helpers\UrlHelper; ?>
 <?php foreach($blogs as $blog): ?>
-    <a href="<?= UrlHelper::getPermalink($blog->getSlug(), [], 'https://thaddeusbibbs.me/dev/mvc-skeleton-1/') ?>"><?= $blog->getTitle() ?></a>
+    <a href="<?= UrlHelper::getPermalink($blog->getSlug()) ?>"><?= $blog->getTitle() ?></a>
 <?php endforeach; ?>
