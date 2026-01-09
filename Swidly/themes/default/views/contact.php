a50b583e11c964d8830789dbc80f519b8bc6319f
Thaddeus Bibbs <tiger2380@gmail.com>
2026-01-09 17:21:14 +0000

    fixed bug

--- Swidly/themes/default/views/contact.php
+++ Swidly/themes/default/views/contact.php
@@ -5,16 +5,12 @@
     @csrf
     <label>First Name</label>
     <input type="text" name="first_name" placeholder="Your name.." value="{{ old('first_name') }}"><br/>
-    <p>{{ $errors->get('first_name') }}</p>
     <label>Last Name</label>
     <input type="text" name="last_name" placeholder="Your last name.." value="{{ old('last_name') }}"><br/>
-    <p>{{ $errors->get('last_name') }}</p>
     <label>Email</label>
     <input type="email" name="email" placeholder="Your email" value="{{ old('email') }}"><br/>
-    <p>{{ $errors->get('email') }}</p>
     <label>Subject</label>
     <textarea name="subject" placeholder="Write something.." style="height:200px">{{ old('subject') }}</textarea><br/>
-    <p>{{ $errors->get('subject') }}</p>
     <input type="submit" value="Submit">
 </form>
 <x-footer />
